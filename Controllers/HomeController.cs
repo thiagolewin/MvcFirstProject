32b60cd12242003a7ff05049ca22a4c53508d05c
THIAGO NICOLÁS LEWIN <47393575@ort>
2023-05-24 15:52:27 -0300

    Entrega

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,36 +22,14 @@ public class HomeController : Controller
     }
     public IActionResult SelectIndumentaria()
     {
-        string[] equipos = new string[10];
-        equipos[0] = "River";
-        equipos[1] = "Sanlorenzo";
-        equipos[2] = "Talleres";
-        equipos[3] = "Defensayjusticia";
-        equipos[4] = "Estudiantes";
-        equipos[5] = "Rosariocentral";
-        equipos[6] = "Belgrano";
-        equipos[7] = "Lanus";
-        equipos[8] = "Argentinosjuniors";
-        equipos[9] = "Boca";
-        ViewBag.ListaEquipos= Equipos.ListaEquipos;
         ViewBag.ListaMedias= Equipos.ListaMedias;
         ViewBag.ListaPantalones= Equipos.ListaPantalones;
         ViewBag.ListaRemeras= Equipos.ListaRemeras;
-        ViewBag.Equipos = equipos;
         return View();
     }
 
     public IActionResult GuardarIndumentaria (string Equipo, int Media, int Pantalon, int Remera)
     {
-    /*    Equipos.ListaEquipos = new List<string>();
-        Equipos.ListaMedias = new List<string>();
-        Equipos.ListaPantalones = new List<string>();
-        Equipos.ListaRemeras = new List<string>();
-        Equipos.EquiposIndumentaria = new Dictionary<string, Indumentaria>();
-        Equipos.ListaMedias.Add(Media);
-        Equipos.ListaPantalones.Add(Pantalon);
-        Equipos.ListaRemeras.Add(Remera);*/
-
         Indumentaria indu = new Indumentaria(Equipos.ListaMedias[Media],Equipos.ListaPantalones[Pantalon],Equipos.ListaRemeras[Remera]);
         Equipos.IngresarIndumentaria(Equipo,indu);  
         return RedirectToAction("Index");
--- Models/equipos.cs
+++ Models/equipos.cs
@@ -1,15 +1,24 @@
 static public class Equipos {
-    public static List<string> ListaEquipos {get;  set;}
-    public static List<string> ListaMedias {get;  set;} //inicialias con los datos
-    public static List<string> ListaPantalones {get;  set;}
-    public static List <string> ListaRemeras {get;  set;}
-    public static Dictionary <string, Indumentaria> EquiposIndumentaria {get;  set;}
+    public static List<string> ListaEquipos {get;  set;}  = new List<string>();
+    public static List<string> ListaMedias {get;  set;} = new List<string>{"River","Sanlorenzo","Talleres","Defensayjusticia","Estudiantes","Rosariocentral","Belgrano","Lanus","Argentinosjuniors","Boca"};
+    public static List<string> ListaPantalones {get;  set;} = new List<string>{"River","Sanlorenzo","Talleres","Defensayjusticia","Estudiantes","Rosariocentral","Belgrano","Lanus","Argentinosjuniors","Boca"};
+    public static List <string> ListaRemeras {get;  set;} = new List<string>{"River","Sanlorenzo","Talleres","Defensayjusticia","Estudiantes","Rosariocentral","Belgrano","Lanus","Argentinosjuniors","Boca"};
+    public static Dictionary <string, Indumentaria> EquiposIndumentaria {get;  set;} = new Dictionary<string, Indumentaria>();
 
     public static bool IngresarIndumentaria(string equipoSeleccionado, Indumentaria item) {
 
-
-
+    bool existe = false;
+    foreach(string elemento in ListaEquipos) {
+        if (equipoSeleccionado == elemento) {
+            existe = true;
+        }
+    }
+    if (!existe) {
+    ListaEquipos.Add(equipoSeleccionado);
     EquiposIndumentaria.Add(equipoSeleccionado, item);
+    } else {
+    EquiposIndumentaria[equipoSeleccionado] = item;
+    }
     return true;
     }
 
